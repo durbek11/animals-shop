258f30a14b039ef6c2755a9a33d8f64bcf8e3109
Durbek <pattoyevd@gmail.com>
2022-09-24 23:00:08 +0500

    inital commits

---
+++ README.md
@@ -0,0 +1,1 @@
+# game-company-website
---
+++ main.js
@@ -0,0 +1,7 @@
+
+new WOW().init();
+
+function myyFunction(){
+btn.classList.remove(`active`);
+document.getElementById(`btnGroupDrop1`).classList.add(`active`);
+}
